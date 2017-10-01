4d6f30fdff944b029ae12649e880105f585becfc
Marek L <nospam.keram@gmail.com>
2017-10-01 14:07:34 +0100

    Add and update benchmarks

--- test/csv_cell_sanitizers/excel_injection_bench.rb
+++ test/csv_cell_sanitizers/excel_injection_bench.rb
@@ -5,19 +5,36 @@ require 'benchmark'
 require 'csv'
 
 module CsvCellSanitizers
+  class SaveCSV < CSV
+    include(ExcelInjection)
+  end
+
   class ExcelInjectionBenchmark < Minitest::Benchmark
     def bench_performance_against_non_sanitized_csv
       Benchmark.bm(10) do |reporter|
         generate_standard(reporter, rows)
         generate_extended(reporter, rows)
+        generate_if_else_everything(reporter, rows)
+        generate_if_else_with_sub(reporter, rows)
+        generate_safecsv(reporter, rows)
       end
     end
 
     def generate_standard(reporter, rows)
       CSV.generate do |csv|
         reporter.report('standard') do
-          rows.each do |item|
-            csv << item
+          rows.each do |row|
+            csv << row
+          end
+        end
+      end
+    end
+
+    def generate_safecsv(reporter, rows)
+      SaveCSV.generate do |csv|
+        reporter.report('safecvs') do
+          rows.each do |row|
+            csv << row
           end
         end
       end
@@ -26,19 +43,51 @@ module CsvCellSanitizers
     def generate_extended(reporter, rows)
       CSV.generate do |csv|
         csv.extend(ExcelInjection)
-
         reporter.report('extended') do
-          rows.each do |item|
-            csv << item
+          rows.each do |row|
+            csv << row
+          end
+        end
+      end
+    end
+
+    def generate_if_else_everything(reporter, rows)
+      sanitize = true
+      CSV.generate do |csv|
+        reporter.report('if_els_ev') do
+          rows.each do |row|
+            csv << if sanitize
+                     row.map { |item| "'#{item}" }
+                   else
+                     row
+                   end
+          end
+        end
+      end
+    end
+
+    def generate_if_else_with_sub(reporter, rows)
+      sanitize = true
+      CSV.generate do |csv|
+        reporter.report('if_els_s') do
+          rows.each do |row|
+            csv << if sanitize
+                     row.map { |item| item.to_s.sub(/(\A=)(?=[^=]+)/, "'=") }
+                   else
+                     row
+                   end
           end
         end
       end
     end
 
     def rows
-      Array.new(10_024, [
-                  [:safe, 'safe string'],
-                  [:evil, '=3+3']
+      Array.new(100_024, [
+                  'safe string',
+                  '=3+3',
+                  '=afdsa+3232',
+                  1337,
+                  :symbol
                 ])
     end
   end
